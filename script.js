cc30223be02c7a9ca14b362b17f85bb2610fe575
Amber <69759137+xoMishka@users.noreply.github.com>
2023-04-02 09:37:23 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// 1. Write a function that reverse a number
+
+// 2. Write a function that checks whether a passed string is a palindrome or not?
+// Palindrome = word, phrase or sequence that reads the same backwards as forward
+// i.e. racecar, madam, nurses run
+
+// 3. Write a function that generates all combinations of a string
+// i.e. 'dog' output 'd, do, dog, o, og, g'
+
+// 4. Write a function that returns a passed string with letters in alphabetical order
+// i.e. 'webmaster' output 'abeemrstw' assume punctuation and number symbols are
+// not included in the passed string
+
+// 5. Write a function that accepts a string as a parameter and converts the first
+// letter of each word of the string in upper case
+// i.e. 'the quick brown fox' output 'The Quick Brown Fox'
+
+// 6. Write a function that accepts a string as a parameter and finds the
+// longest word within the string, i.e. 'Web Development Tutorial' output 'Development'
+
+// 7. Write a function that accepts a string as a parameter and counts the number of
+// vowels within the string, do not include y as a vowel, i.e. 'The quick brown fox'
+// output = 5
+
+// 8. Write a function that accepts a number as a parameter and check whether the number
+// is prime or not. A prime number is a natural number greater than 1 that has no
+// positive divisors other than 1 and itself
+
+// 9. Write a function which accepts an argument and returns the type, there are 6
+// possible values that typeof returns: object, boolean, function, number, string,
+// and undefined
+
+// 10. Write a function which returns the n rows by n columns identity matrix
+
+// 11. Write a function which will take an array of numbers stored and find the second
+// lowest and second greatest numbers, respectively, i.e. [1, 2, 3, 4, 5] output 2, 4
+
+// 12. Perfect Number TLDR
+
+// 13. Write a function to compute the factors of a positive integer
+
+// 14. Write a function to convert an amount to coins TLDR
+
+// 15. Write a function to compute the value of b^n where n is the exponent and b is
+// the base. Accept b and n from the user and display the result
+
+// 16. Write a function to extract unique characters from a string
+
+// 17. Write a function to get the number of occurrences of each letter in specified
+// string
+
+// 18. Write a function for searching JavaScript arrays with a binary search
+// A binary search searches by splitting an array into smaller and smaller chunks
+// until it finds the desired value
+
+// 19. Write a function that returns array elements larger than a number
+
+// 20. Write a function that generates a string id (specified length) of random
+// characters
+
+// 21. Write a function to get all possible subset with a fixed length (i.e. 2)
+// combinations in an array, i.e. [1, 2, 3] and subset length is 2
+// output [[2, 1], [3,1], [3,2], [3,2,1]]
+
+// 22. Write a function that accepts two arguments, a string and a letter and the
+// function will count the number of occurrences of the specified letter within the
+// string, i.e. 'w3resource.com', 'o' and output = 2
+
+// 23.
