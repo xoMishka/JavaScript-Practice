cf27c705f50e754f3f67be6c5fa10128f5ce58ed
Amber <69759137+xoMishka@users.noreply.github.com>
2023-04-02 13:48:28 -0600

    Stopped at dog age

--- script.js
+++ script.js
@@ -79,7 +79,7 @@
         return Math.PI * radius * radius;
     }
 
-    console.log(circleArea(3));     // 28.274
+    console.log(circleArea(3));             // 28.274
     console.log(circleArea(1.492705));      // 6.9999
 
 // Define a function called 'multiplyFive' which accepts a number and returns
@@ -96,6 +96,13 @@
 name, geographic location, job title. It outputs your fortune to the screen like so: 
 "You will be a X in Y, and married to Z with N kids." Call that function 3 times with 
 3 different values for the arguments. */
+    function tellFortune(numChildren, partnerName, geoLocation, jobTitle) {
+        var future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ' and married to '
+        + partnerName + ' ' + 'with ' + numChildren + ' kids.';
+        console.log(future);
+    }
+
+    console.log(tellFortune('2', 'Xander', 'Alaska', 'Web Developer'));
 
 /* You know how old your dog is in human years, but what about dog years? Calculate it!
 Write a function named calculateDogAge that: takes 1 argument: your puppy's age, calculates 
@@ -103,6 +110,17 @@ your dog's age based on the conversion rate of 1 human year to 7 dog years, and 
 the result to the screen like so: "Your doggie is NN years old in dog years!" Call the 
 function three times with different sets of values. Bonus: Add an additional argument to 
 the function that takes the conversion rate of human to dog years. */
+    function calculateDogAge(dogAge, x) {
+        // dogAge
+
+        // calculate dog age (1 human year = 7 dog years)
+
+        // output the result: "Your doggie is x years old in dog years!"
+        console.log('Your doggie is ' + x + ' years old in dog years!');
+    }
+
+    // call the function 3 times
+
 
 /* Create 2 functions that calculate properties of a circle, using the definitions here.
 Create a function called calcCircumfrence: Pass the radius to the function. Calculate the 
