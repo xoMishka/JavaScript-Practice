38209878657b3f94d2030596e02f85efa4cf22b7
Amber <69759137+xoMishka@users.noreply.github.com>
2023-05-13 23:12:00 -0600

    Practice number variables

---
+++ practice.js
@@ -0,0 +1,24 @@
+// Declare a variable num1 and assign it the value 5. Declare another variable num2 and assign it the value 7. Write a code snippet to calculate the sum of num1 and num2 and store the result in a variable called sum.
+let num1 = 5;
+let num2 = 7;
+let sum = num1 + num2;
+
+// Declare a variable radius and assign it a value of 10. Write a code snippet to calculate the circumference of a circle with the given radius and store the result in a variable called circumference. Assume the value of π (pi) to be 3.14159.
+let radius = 10;
+
+
+// Write a code snippet to convert a temperature in Celsius to Fahrenheit. Declare a variable celsius and assign it a value. Calculate the corresponding temperature in Fahrenheit and store it in a variable called fahrenheit. The conversion formula is: fahrenheit = celsius * 9/5 + 32.
+
+// Declare a variable price and assign it a value. Write a code snippet to calculate the final price after applying a discount of 20%. Store the result in a variable called finalPrice.
+
+// Write a code snippet to find the largest number among three given numbers. Declare three variables num1, num2, and num3, and assign them different values. Find the largest number among them and store it in a variable called largestNumber.
+
+// Write a code snippet to calculate the factorial of a given number. Declare a variable num and assign it a positive integer value. Calculate the factorial of num and store the result in a variable called factorial. The factorial of a number is the product of all positive integers less than or equal to that number.
+
+// Write a code snippet to check if a given number is even or odd. Declare a variable num and assign it a value. Use an if-else statement to determine if the number is even or odd, and store the result in a variable called isEven. The variable should be true if the number is even and false if it is odd.
+
+// Write a code snippet to calculate the sum of all even numbers from 1 to 100. Declare a variable sum and initialize it to 0. Use a loop to iterate through all numbers from 1 to 100. If a number is even, add it to the sum variable. Finally, output the value of sum.
+
+// Write a code snippet to generate the Fibonacci sequence up to a given number of terms. Declare a variable numTerms and assign it a positive integer value. Generate the Fibonacci sequence up to numTerms and store the numbers in an array called fibonacciSeq. The Fibonacci sequence starts with 0 and 1, and each subsequent number is the sum of the two previous numbers.
+
+// Write a code snippet to round a decimal number to the nearest integer. Declare a variable decimal and assign it a decimal value. Round the decimal to the nearest integer and store the result in a variable called roundedValue.
