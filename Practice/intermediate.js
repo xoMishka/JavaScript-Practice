93240799916c5e8efa49fd7b0389fdfa376805f2
Amber <69759137+xoMishka@users.noreply.github.com>
2023-05-20 17:33:52 -0600

    yes

--- Practice/intermediate.js
+++ Practice/intermediate.js
@@ -1,27 +1,6 @@
 /***** NUMBER VARIABLES *****/
 
-// Declare a variable num1 and assign it the value 5. Declare another variable num2 and assign it the value 7. Write a code snippet to calculate the sum of num1 and num2 and store the result in a variable called sum.
-
-// Declare a variable radius and assign it a value of 10. Write a code snippet to calculate the circumference of a circle with the given radius and store the result in a variable called circumference. Assume the value of π (pi) to be 3.14159. C = 2πr
-
-// Write a code snippet to convert a temperature in Celsius to Fahrenheit. Declare a variable celsius and assign it a value. Calculate the corresponding temperature in Fahrenheit and store it in a variable called fahrenheit. The conversion formula is: fahrenheit = celsius * 9/5 + 32.
-
-// Declare a variable price and assign it a value. Write a code snippet to calculate the final price after applying a discount of 20%. Store the result in a variable called finalPrice.
-
-// Write a code snippet to find the largest number among three given numbers. Declare three variables num1, num2, and num3, and assign them different values. Find the largest number among them and store it in a variable called largestNumber.
-
 // Write a code snippet to calculate the factorial of a given number. Declare a variable num and assign it a positive integer value. Calculate the factorial of num and store the result in a variable called factorial. The factorial of a number is the product of all positive integers less than or equal to that number, i.e. 5 = 1 * 2 * 3 * 4 * 5
-function factorial() {
-    var num = 10;
-    var factorial = 1;
-    for (let i = 1; i <= num; i++) {
-        factorial *= i;
-    }
-    return factorial;
-}
-
-console.log(factorial());
-
 function calculateFactorial() {
     let num = 5;
     let factorial = 1;
@@ -55,10 +34,6 @@ console.log(evenOrOdd());
 // Write a code snippet to round a decimal number to the nearest integer. Declare a variable decimal and assign it a decimal value. Round the decimal to the nearest integer and store the result in a variable called roundedValue.
 
 /***** STRING VARIABLES *****/
-
-// Declare a variable name and assign it your name as a string. Print out the value of name to the console.
-
-// Declare a variable message and assign it a string that says "Hello, World!". Print out the value of message to the console.
 
 // Declare a variable sentence and assign it a string that contains multiple words. Write a code snippet to count the number of words in the sentence and store the result in a variable called wordCount.
 var sentence = "This is a sample sentence.";
