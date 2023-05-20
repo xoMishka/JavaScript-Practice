07ba445aa32bff6d7dfe8477fe3b3600b0d2126d
Amber <69759137+xoMishka@users.noreply.github.com>
2023-05-20 17:33:42 -0600

    yes

---
+++ Practice/edabit.js
@@ -0,0 +1,13 @@
+// Create a function that takes two numbers as arguments and returns their sum, i.e. addition(3, 2) --> 5
+function addition(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+
+console.log(addition(3, 4));    // 7
+
+// Write a function that converts hours into seconds, i.e. 2 = 7200
+// 2 hours = 60 minutes * 60 = 3600 seconds per hour * 2 = 7200
+function howManySeconds() {
+
+}
