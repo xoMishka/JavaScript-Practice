3644656560c7395b5c4a81bb6d744c1b8b542460
Amber <69759137+xoMishka@users.noreply.github.com>
2023-05-20 14:20:28 -0600

    removed

--- practice.js
+++ practice.js
@@ -1,54 +1,14 @@
 /***** NUMBER VARIABLES *****/
 
 // Declare a variable num1 and assign it the value 5. Declare another variable num2 and assign it the value 7. Write a code snippet to calculate the sum of num1 and num2 and store the result in a variable called sum.
-let num1 = 5;
-let num2 = 7;
-let sum = num1 + num2;
 
 // Declare a variable radius and assign it a value of 10. Write a code snippet to calculate the circumference of a circle with the given radius and store the result in a variable called circumference. Assume the value of π (pi) to be 3.14159. C = 2πr
-function findCircumference() {
-    let radius = 10;
-    let circumference = 2 * 3.14159 * radius;
-    return circumference; // 62.8318
-}
-
-console.log(findCircumference());
 
 // Write a code snippet to convert a temperature in Celsius to Fahrenheit. Declare a variable celsius and assign it a value. Calculate the corresponding temperature in Fahrenheit and store it in a variable called fahrenheit. The conversion formula is: fahrenheit = celsius * 9/5 + 32.
-function toFahrenheit() {
-    let celsius = 54;
-    let fahrenheit = celsius * 9/5 + 32;
-    return fahrenheit; // 129.2
-}
-
-console.log(toFahrenheit());
 
 // Declare a variable price and assign it a value. Write a code snippet to calculate the final price after applying a discount of 20%. Store the result in a variable called finalPrice.
-function finalPrice2() {
-    var price = 40;
-    var discount = .20;
-    var finalPrice = price - price * discount;
-    return finalPrice;
-}
-
-console.log(finalPrice2());
-
-function finalPrice() {
-    let price = 20;
-    let finalPrice = price - price * .20;
-    return finalPrice;
-}
-
-console.log(finalPrice());
 
 // Write a code snippet to find the largest number among three given numbers. Declare three variables num1, num2, and num3, and assign them different values. Find the largest number among them and store it in a variable called largestNumber.
-let firstNum = 10;
-let secNum = 25;
-let thirdNum = 15;
-
-let largestNumber = Math.max(firstNum, secNum, thirdNum);
-
-console.log(largestNumber);
 
 // Write a code snippet to calculate the factorial of a given number. Declare a variable num and assign it a positive integer value. Calculate the factorial of num and store the result in a variable called factorial. The factorial of a number is the product of all positive integers less than or equal to that number.
 function calculateFactorial() {
@@ -65,7 +25,17 @@ function calculateFactorial() {
 console.log(calculateFactorial());
 
 // Write a code snippet to check if a given number is even or odd. Declare a variable num and assign it a value. Use an if-else statement to determine if the number is even or odd, and store the result in a variable called isEven. The variable should be true if the number is even and false if it is odd.
+function evenOrOdd() {
+    var num = 2;
+    if (num % 2 === 0) {
+        isEven = true;
+    }
+    else {
+        isEven = false;
+    }
+}
 
+console.log(evenOrOdd());
 
 // Write a code snippet to calculate the sum of all even numbers from 1 to 100. Declare a variable sum and initialize it to 0. Use a loop to iterate through all numbers from 1 to 100. If a number is even, add it to the sum variable. Finally, output the value of sum.
 
@@ -76,12 +46,8 @@ console.log(calculateFactorial());
 /***** STRING VARIABLES *****/
 
 // Declare a variable name and assign it your name as a string. Print out the value of name to the console.
-let name = "Amber";
-console.log(name);
 
 // Declare a variable message and assign it a string that says "Hello, World!". Print out the value of message to the console.
-let message = "Hello, World!";
-console.log(message);
 
 // Declare a variable sentence and assign it a string that contains multiple words. Write a code snippet to count the number of words in the sentence and store the result in a variable called wordCount.
 var sentence = "This is a sample sentence.";
